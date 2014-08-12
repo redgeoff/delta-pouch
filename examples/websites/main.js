bcde201660f276ccb0e2b72f12b909da8b83d5e9
Geoff Cox <redgeoff@gmail.com>
2014-08-11 22:02:42 -0700

    initial commit

---
+++ examples/websites/main.js
@@ -0,0 +1,169 @@
+'use strict';
+
+/* exported purge, editSite, removeSite, saveSite, cleanup */
+/* global PouchDB, Promises, Promise */
+
+var sites = [], curSite = null, deletions = {}, db = new PouchDB('websites'),
+    remoteCouch = (location.search ? 'https://delta-pouch.iriscouch.com'
+      : 'http://127.0.0.1:5984') + '/websites';
+
+function setFormValues(site) {
+  document.getElementById('name').value = site.name ? site.name : '';
+  document.getElementById('url').value = site.url ? site.url : '';
+  curSite = db.clone(site); // save so we can identify changes later
+}
+
+function getFormValues() {
+  return { name: document.getElementById('name').value, url: document.getElementById('url').value };
+}
+
+function setFormVisible(visible) {
+  document.getElementById('form').style.display = visible ? 'block' : 'none';
+  document.getElementById('newButton').style.display = !visible ? 'block' : 'none';
+}
+
+function getItemHtml(site) {
+  return '<button onclick="editSite(\'' + site.$id + '\')">Edit</button>' +
+    '<button onclick="removeSite(\'' + site.$id + '\')">Delete</button> ' +
+    site.name + ': ' + site.url;
+}
+
+function addItem(site) {
+  var siteList = document.getElementById('siteList'), li = document.createElement('li');
+  li.id = site.$id;
+  li.innerHTML = getItemHtml(site);
+  siteList.appendChild(li);
+}
+
+function updateItem(site) {
+  var item = document.getElementById(site.$id);
+  item.innerHTML = getItemHtml(site);
+}
+
+function deleteItem(id) {
+  document.getElementById('siteList').removeChild(document.getElementById(id));
+}
+
+function indexOf(id) {
+  for (var i in sites) {
+    if (sites[i].$id === id) {
+      return i;
+    }
+  }
+  return null;    
+}
+
+function getSite(id) {
+  var i = indexOf(id);
+  return i === null ? null : sites[i];
+}
+
+function deleteSite(id) {
+  var i = indexOf(id);
+  if (i !== null) {
+    sites.splice(i, 1);
+  }
+}
+
+function updateSite(site) {
+  var i = indexOf(site.$id);
+  if (i !== null) {
+    sites[i] = site; 
+  }
+}
+
+function addSiteAndItem(site) {
+  sites.push(site);
+  addItem(site);
+}
+
+function updateSiteAndItem(site) {
+  updateSite(site);
+  updateItem(site);
+}
+
+function deleteSiteAndItem(id) {
+  deleteSite(id);
+  deleteItem(id);
+}
+
+function editSite(id) {
+  setFormValues(id ? getSite(id) : {});
+  setFormVisible(true);
+}
+
+function removeSite(id) {
+  db.delete(id).then(function () {
+    deleteSiteAndItem(id);
+  });
+}
+
+function saveChanges() {
+  db.saveChanges(curSite, getFormValues(), function (newSite) {
+    updateItem(newSite);      
+  });
+}
+
+function saveNew() {
+  db.save(getFormValues()).then(function (site) {
+    site.$id = site.id;
+    addSiteAndItem(site);
+  });
+}
+
+function saveSite() {
+  if (curSite.$id) { // existing?
+    saveChanges();
+  } else {
+    saveNew();
+  }
+  setFormVisible(false);
+}
+
+function onCreate(object) {
+  db.onCreate(object, getSite, deletions, addSiteAndItem, updateSiteAndItem, deleteSiteAndItem);
+}
+
+db.info(function (err, info) {
+  db.changes({
+      since: info.update_seq,
+      live: true
+    })
+    .on('create', onCreate);
+});
+
+var opts = { live: true };
+db.replicate.to(remoteCouch, opts);
+db.replicate.from(remoteCouch, opts);
+
+db.all().then(function (docs) {
+  for (var i in docs) {
+    addSiteAndItem(docs[i]);
+  }
+});
+
+// NOTE: this function does not cause the UI to update. It is provided only for testing purposes.
+function purge() {
+  return new Promise(function (fulfill) {
+    var promises = [];
+    db.allDocs({include_docs: true}, function (err, doc) {
+      doc.rows.forEach(function (el, i) {
+        db.get(el.doc._id).then(function (object) {
+          promises.push(db.remove(object));
+          if (i === doc.rows.length - 1) { // last element?
+            Promises.all(promises).then(function () {
+              console.log('purge done');
+              fulfill();
+            });
+          }
+        });
+      });
+    });
+  });
+}
+
+function cleanup() {
+  db.cleanup().then(function () {
+    console.log('cleanup done');
+  });
+}
--- test/test.js
+++ test/test.js
@@ -6,8 +6,8 @@ var Pouch = require('pouchdb');
 //
 // your plugin goes here
 //
-var helloPlugin = require('../');
-Pouch.plugin(helloPlugin);
+var deltaPlugin = require('../');
+Pouch.plugin(deltaPlugin);
 
 var chai = require('chai');
 chai.use(require("chai-as-promised"));
@@ -16,7 +16,7 @@ chai.use(require("chai-as-promised"));
 // more variables you might want
 //
 chai.should(); // var should = chai.should();
-require('bluebird'); // var Promise = require('bluebird');
+var Promise = require('bluebird'); // var Promise = require('bluebird');
 
 var dbs;
 if (process.browser) {
@@ -31,6 +31,14 @@ dbs.split(',').forEach(function (db) {
   tests(db, dbType);
 });
 
+function length(obj) {
+  var n = 0;
+  for (var i in obj) { // jshint unused:false
+    n++;
+  }
+  return n;
+}
+
 function tests(dbName, dbType) {
 
   var db;
@@ -42,11 +50,319 @@ function tests(dbName, dbType) {
   afterEach(function () {
     return Pouch.destroy(dbName);
   });
-  describe(dbType + ': hello test suite', function () {
-    it('should say hello', function () {
-      return db.sayHello().then(function (response) {
-        response.should.equal('hello');
+
+  describe(dbType + ': delta test suite', function () {
+
+    // this.timeout(5000);
+
+    it('should clone', function () {
+      var doc = { title: 'take out trash', priority: 'low' }, clonedDoc = db.clone(doc);
+      clonedDoc.title = 'clean dishes';
+      doc.title.should.eql('take out trash');
+    });
+
+    it('should save', function () {
+      var doc = { title: 'take out trash', priority: 'low' };
+      return db.save(doc).then(function (response) {
+        response.should.eql({ ok: true, id: response.id, rev: response.rev});
       });
     });
+
+    it('should delete', function () {
+      return db.delete(123).then(function (response) {
+        response.should.eql({ ok: true, id: response.id, rev: response.rev});
+      });
+    });
+
+    function saveTrash() {
+      return new Promise(function (fulfill) {
+        db.save({ title: 'take out trash' }).then(function (doc) {
+          db.save({ $id: doc.id, priority: 'medium' }).then(function () {
+            db.save({ $id: doc.id, title: 'take out trash and recycling' }).then(function () {
+              db.save({ $id: doc.id, priority: 'high' }).then(function () {
+                fulfill(doc.id);
+              });
+            });
+          });
+        });
+      });
+    }
+
+    function assertAllDocs(objs) {
+      return new Promise(function (fulfill) {
+        db.all().then(function (docs) {
+          length(docs).should.equal(length(objs));
+          for (var i in objs) {
+            assertContains(docs[i], objs[1]);
+          }
+          fulfill();
+        });
+      });
+    }
+
+    function assertContains(obj1, obj2) {
+      for (var i in obj2) {
+        obj2[i].should.equal(obj1[i]);
+      }
+    }
+
+    function failure() {
+      '1'.should.equal('2');
+    }
+
+    function saveDishes() {
+      return new Promise(function (fulfill) {
+        db.save({ priority: 'low' }).then(function (doc) {
+          db.save({ $id: doc.id, title: 'clean dishes' }).then(function () {
+            db.save({ $id: doc.id, title: 'clean & dry dishes' }).then(function () {
+              db.save({ $id: doc.id, priority: 'medium' }).then(function () {
+                fulfill(doc.id);
+              });
+            });
+          });
+        });
+      });
+    }
+
+    function deleteTrash() {
+      return new Promise(function (fulfill) {
+        saveTrash().then(function (trashId) {
+          saveDishes().then(function (dishesId) {
+            db.delete(trashId).then(function () {
+              // save after delete shouldn't change docs
+              db.save({ $id: trashId, title: 'replace trash bag' }).then(function () {
+                var docs = {};
+                docs[dishesId] = { title: 'clean & dry dishes', priority: 'medium' };
+                assertAllDocs(docs).then(function () {
+                  fulfill(dishesId);
+                });
+              });
+            });
+          });
+        });
+      });
+    }
+
+    it('all should call callback even when no docs', function (done) {
+      db.all().then(function (docs) {
+        (typeof docs).should.equal('undefined');
+        done();
+      });
+    });
+
+    it('should update doc and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        return saveTrash().then(function (id) {
+          var docs = {};
+          docs[id] = { title: 'take out trash and recycling', priority: 'high' };
+          assertAllDocs(docs).then(fulfill);
+        });
+      });
+    });
+
+    it('should update docs and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        saveTrash().then(function (trashId) {
+          saveDishes().then(function (dishesId) {
+            var docs = {};
+            docs[trashId] = { title: 'take out trash and recycling', priority: 'high' };
+            docs[dishesId] = { title: 'clean & dry dishes', priority: 'medium' };
+            assertAllDocs(docs).then(fulfill);
+          });
+        });
+      });
+    });
+
+    it('should delete doc and be reflected in all', function () {
+      return deleteTrash();
+    });
+
+    function cleanup() {
+      return new Promise(function (fulfill) {
+        db.cleanup().then(function () {
+          db.allDocs({ include_docs: true }, function (err, doc) {
+            doc.rows.sort(function (a, b) {
+              return a.doc.$createdAt > b.doc.$createdAt;
+            });
+            fulfill(doc);
+          });
+        });
+      });
+    }
+
+    it('should cleanup updates and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        saveTrash().then(function (dishesId) {
+          cleanup().then(function (doc) {
+            assertContains(doc.rows[0].doc,
+              { $id: dishesId, title: 'take out trash and recycling' });
+            assertContains(doc.rows[1].doc,
+              { $id: dishesId, priority: 'high' });
+            fulfill();
+          });
+        });
+      });
+    });
+
+    it('should cleanup deletions and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        saveTrash().then(function (trashId) {
+          db.delete(trashId).then(function () {
+            cleanup().then(function (doc) {
+              doc.rows.length.should.equal(0);
+              fulfill();
+            });
+          });
+        });
+      });
+    });
+
+    it('should cleanup deletions and a following update and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        saveTrash().then(function (trashId) {
+          db.delete(trashId).then(function () {
+            // save after delete shouldn't change docs
+            db.save({ $id: trashId, title: 'replace trash bag' }).then(function () {
+              cleanup().then(function (doc) {
+                doc.rows.length.should.equal(0);
+                fulfill();
+              });
+            });
+          });
+        });
+      });
+    });
+
+    it('should cleanup docs and be reflected in all', function () {
+      return new Promise(function (fulfill) {
+        deleteTrash().then(function (dishesId) {
+          cleanup().then(function (doc) {
+            assertContains(doc.rows[0].doc,
+              { $id: dishesId, title: 'clean & dry dishes' });
+            assertContains(doc.rows[1].doc,
+              { $id: dishesId, priority: 'medium' });
+            fulfill();
+          });
+        });
+      });
+    });
+
+    it('should save changes', function () {
+      return new Promise(function (fulfill) {
+        var item = { $id: 1, title: 'take out trash', priority: 'high'},
+            updates = { title: 'take out recycling', priority: 'high' };
+        db.saveChanges(item, updates, function (response) {
+          assertContains(response,
+            { $id: item.$id, title: updates.title, priority: item.priority });
+          var docs = {};
+          docs[item.$id] = { $id: item.$id, title: updates.title };
+          assertAllDocs(docs).then(fulfill);
+        });
+      });
+    });
+
+    it('should save no changes', function () {
+      var item = { $id: 1, title: 'take out trash', priority: 'high'},
+          updates = {};
+      db.saveChanges(item, updates, function () {
+        failure();
+      });
+    });
+
+    it('should onCreate onCreate', function () {
+      var item = { title: 'take out trash' };
+      db.save(item).then(function (object) {
+        var deletions = {};
+        function getItem() {
+          return null;
+        }
+        function onCreate(doc) {
+          assertContains(doc, { $id: object.id, title: item.title });
+        }
+        db.onCreate(object, getItem, deletions, onCreate);
+      });
+    });
+
+    it('should onCreate onCreate ignore deletions', function () {
+      var item = { title: 'take out trash' };
+      db.save(item).then(function (object) {
+        var deletions = {};
+        deletions[object.id] = true;
+        function getItem() {
+          return null;
+        }
+        function onCreate() {
+          failure();
+        }
+        db.onCreate(object, getItem, deletions, onCreate);
+      });
+    });
+
+    it('should onCreate onUpdate', function (done) {
+      var item1 = { title: 'take out trash' };
+      db.save(item1).then(function (object1) {
+        var item2 = { $id: object1.id, title: 'take out recycling' };
+        db.save(item2).then(function (object2) {
+          var deletions = {};
+          function getItem() {
+            return { $id: object1.id, title: item1.title };
+          }
+          function onUpdate(doc) {
+            assertContains(doc, { $id: item2.$id, title: item2.title });
+            done();
+          }
+          db.onCreate(object2, getItem, deletions, null, onUpdate);
+        });
+      });
+    });
+
+    it('should onCreate onUpdate ignore deletions', function () {
+      var item = { title: 'take out trash' };
+      db.save(item).then(function (object) {
+        var deletions = {};
+        deletions[object.id] = true;
+        function getItem() {
+          return { $id: object.id, title: item.title };
+        }
+        function onUpdate() {
+          failure();
+        }
+        db.onCreate(object, getItem, deletions, null, onUpdate);
+      });
+    });
+
+    it('should onCreate onDelete', function (done) {
+      var item = { $deleted: true };
+      db.save(item).then(function (object) {
+        var deletions = {};
+        function getItem() {
+          return { $id: object.id, title: item.title };
+        }
+        function onDelete(id) {
+          id.should.equal(object.id);
+          var dels = {};
+          dels[id] = true;
+          assertContains(deletions, dels);
+          done();
+        }
+        db.onCreate(object, getItem, deletions, null, null, onDelete);
+      });
+    });
+
+    it('should onCreate onDelete no item', function () {
+      var item = { $deleted: true };
+      db.save(item).then(function (object) {
+        var deletions = {};
+        function getItem() {
+          return null;
+        }
+        function onDelete() {
+          failure();
+        }
+        db.onCreate(object, getItem, deletions, null, null, onDelete);
+      });
+    });
+
+    // TODO: test simulatenous client updates/deletes
   });
 }
