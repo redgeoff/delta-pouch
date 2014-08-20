e8751b5080f6a742568609486aa06bcbb2afe27d
Geoff Cox <redgeoff@gmail.com>
2014-08-19 18:58:21 -0700

    fix(put): bug in pouch requires catch of put even though only creating doc

--- index.js
+++ index.js
@@ -55,7 +55,14 @@ function save(db, doc) {
   if (doc.$id) { // update?
     // this format guarantees the docs will be retrieved in order they were created
     doc._id = doc.$id + '_' + doc.$createdAt;
-    return db.put(doc);
+    return db.put(doc).catch(function (err) {
+      // It appears there is a bug in pouch that causes a doc conflict even though we are creating a
+      // new doc
+      /* istanbul ignore if */
+      if (err.status !== 409) {
+        throw err;
+      }
+    });
   } else { // new
     return db.post(doc);
   }
